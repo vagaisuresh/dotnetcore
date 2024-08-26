844e1bba6b9084b7be5c13d719b62cd2e47af132
tiaanosoft <tiaanosoft@outlook.com>
2024-08-26 16:45:34 +0530

    Add project files.

---
+++ mvc-series/MvcSeries/GettingStarted/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GettingStarted.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
