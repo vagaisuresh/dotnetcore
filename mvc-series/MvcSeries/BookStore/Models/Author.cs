266b1905b35f6c5b8049992982d266c5e9e58ecf
vagaisuresh <vagaisuresh@yahoo.co.in>
2024-08-27 14:45:05 +0530

    Author model

---
+++ mvc-series/MvcSeries/BookStore/Models/Author.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Author Name")]
+        [Required(ErrorMessage = "Author Name is required.")]
+        [StringLength(maximumLength: 50, MinimumLength = 3)]
+        public string? Name { get; set; }
+    }
+}
