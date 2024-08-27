bcdfdfaf500b69e3bb4c94ff77cd016328447007
vagaisuresh <vagaisuresh@yahoo.co.in>
2024-08-27 10:03:33 +0530

    BookStore first commit

---
+++ mvc-series/MvcSeries/BookStore/Controllers/BooksController.cs
@@ -0,0 +1,141 @@
+﻿using BookStore.Data;
+using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly BookStoreContext _context;
+
+        public BooksController(BookStoreContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Book.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Book
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Genre,Price,PublishDate")] Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(book);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(book);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Book.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return View(book);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Genre,Price,PublishDate")] Book book)
+        {
+            if (id != book.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(book);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BookExists(book.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(book);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Book
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var book = await _context.Book.FindAsync(id);
+            if (book != null)
+            {
+                _context.Book.Remove(book);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BookExists(int id)
+        {
+            return _context.Book.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ mvc-series/MvcSeries/BookStore/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Book Title")]
+        [Required(ErrorMessage = "Book Title is required.")]
+        public string? Title { get; set; }
+
+        public string? Genre { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Range(1, 100)]
+        public decimal Price { get; set; }
+
+        [Display(Name = "Publish Date")]
+        [DataType(DataType.Date)]
+        public DateTime PublishDate { get; set; }
+    }
+}
