ad6b3e7a874f1de633072ef6733db9c8cdf02362
vagaisuresh <vagaisuresh@yahoo.co.in>
2024-08-31 18:02:41 +0530

    Validation

--- mvc-series/MvcSeries/BookStore/Models/Book.cs
+++ mvc-series/MvcSeries/BookStore/Models/Book.cs
@@ -14,10 +14,12 @@ namespace BookStore.Models
 
         [DataType(DataType.Currency)]
         [Range(1, 1000)]
+        [Required(ErrorMessage = "Price is required.")]
         public decimal Price { get; set; }
 
         [Display(Name = "Publish Date")]
         [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Publish Date is required.")]
         public DateTime PublishDate { get; set; }
 
         public required ICollection<BookAuthor> BookAuthors { get; set; }
