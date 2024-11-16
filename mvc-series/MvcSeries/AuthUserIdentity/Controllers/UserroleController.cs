03f51ecfae8004b8e09d7981a51f0de700ae1977
vagaisuresh <vagaisuresh@yahoo.co.in>
2024-11-16 17:48:53 +0530

    User Role

--- mvc-series/MvcSeries/AuthUserIdentity/Controllers/UserroleController.cs
+++ mvc-series/MvcSeries/AuthUserIdentity/Controllers/UserroleController.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using AuthUserIdentity.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using AuthUserIdentity.Models;
 
 namespace AuthUserIdentity.Controllers
 {
@@ -18,13 +13,11 @@ namespace AuthUserIdentity.Controllers
             _context = context;
         }
 
-        // GET: Userrole
         public async Task<IActionResult> Index()
         {
             return View(await _context.Userrolemasters.ToListAsync());
         }
 
-        // GET: Userrole/Details/5
         public async Task<IActionResult> Details(short? id)
         {
             if (id == null)
@@ -42,15 +35,11 @@ namespace AuthUserIdentity.Controllers
             return View(userrolemaster);
         }
 
-        // GET: Userrole/Create
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Userrole/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,RoleName,Description,IsActive,IsRemoved")] Userrolemaster userrolemaster)
@@ -64,7 +53,6 @@ namespace AuthUserIdentity.Controllers
             return View(userrolemaster);
         }
 
-        // GET: Userrole/Edit/5
         public async Task<IActionResult> Edit(short? id)
         {
             if (id == null)
@@ -80,9 +68,6 @@ namespace AuthUserIdentity.Controllers
             return View(userrolemaster);
         }
 
-        // POST: Userrole/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(short id, [Bind("Id,RoleName,Description,IsActive,IsRemoved")] Userrolemaster userrolemaster)
@@ -115,7 +100,6 @@ namespace AuthUserIdentity.Controllers
             return View(userrolemaster);
         }
 
-        // GET: Userrole/Delete/5
         public async Task<IActionResult> Delete(short? id)
         {
             if (id == null)
@@ -133,7 +117,6 @@ namespace AuthUserIdentity.Controllers
             return View(userrolemaster);
         }
 
-        // POST: Userrole/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(short id)
--- mvc-series/MvcSeries/AuthUserIdentity/Models/Userrolemaster.cs
+++ mvc-series/MvcSeries/AuthUserIdentity/Models/Userrolemaster.cs
@@ -1,9 +1,17 @@
-﻿namespace AuthUserIdentity.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AuthUserIdentity.Models
 {
+    [Table("Userrolemaster")]
     public class Userrolemaster
     {
         public short Id { get; set; }
+        [Required]
+        [StringLength(30)]
         public string? RoleName { get; set; }
+        [Required]
+        [StringLength(100)]
         public string? Description { get; set; }
         public bool IsActive { get; set; }
         public bool IsRemoved { get; set; }
